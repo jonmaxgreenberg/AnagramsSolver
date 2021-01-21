64700f13320a26d1a44451992a5de20748f86b0b
Jonathan Greenberg <jgreenb2@mail.yu.edu>
2021-01-20 22:23:06 -0500

    refactor: change html file name from greet to results

--- anagrams_app.py
+++ anagrams_app.py
@@ -7,30 +7,20 @@ app = Flask(__name__)
 def home():
  return render_template('home.html')
 
-HOME_HTML = """
- <html><body>
-     <h2>Welcome to the Anagram Solver!</h2>
-     <form action="/greet">
-         What word would you like to steal? <input type='text' name='base word'><br>
-         How many letters are you willing to add on? <input type='text' name='number of letters'><br>
-         <input type='submit' value='Continue'>
-     </form>
- </body></html>"""
-
-@app.route('/greet')
-def greet():
+@app.route('/results')
+def results():
  base_word = request.args.get('base word', '')
  num_letters = request.args.get('number of letters', '')
  print(num_letters)
  if base_word == '' or num_letters == '':  # if there was no base word given
     base_word = 'Fill out all the info please!'  # you gotta give a base word!
     result_set = 'Please try again!'
-    return GREET_HTML.format(base_word, result_set) #CHANEGE THIS
+    return render_template('results.html', base_word=base_word, answers=result_set)
 
  # run algo here put in base_word and num_letters
  result_set = anagramssolver.main(base_word, num_letters)
  print('result set: ' + str(result_set))
- return render_template('greet.html', base_word=base_word, answers=result_set)
+ return render_template('results.html', base_word=base_word, answers=result_set)
 
 
 
