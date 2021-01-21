d2c7c79948ce38ed33751bee45ea8ac1cf788ee9
Jonathan Greenberg <jgreenb2@mail.yu.edu>
2021-01-20 22:15:01 -0500

    style: update html display to show results in table

--- anagrams_app.py
+++ anagrams_app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import anagramssolver
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
- return HOME_HTML
+ return render_template('home.html')
 
 HOME_HTML = """
  <html><body>
@@ -25,20 +25,21 @@ def greet():
  if base_word == '' or num_letters == '':  # if there was no base word given
     base_word = 'Fill out all the info please!'  # you gotta give a base word!
     result_set = 'Please try again!'
-    return GREET_HTML.format(base_word, result_set)
+    return GREET_HTML.format(base_word, result_set) #CHANEGE THIS
 
  # run algo here put in base_word and num_letters
  result_set = anagramssolver.main(base_word, num_letters)
  print('result set: ' + str(result_set))
- return GREET_HTML.format(base_word, result_set)
+ return render_template('greet.html', base_word=base_word, answers=result_set)
 
 
-GREET_HTML = """
- <html><body>
-     <h2>The word {0} can be stolen in these ways!</h2>
-     {1}
- </body></html>
- """
+
+# GREET_HTML = """
+#  <html><body>
+#      <h2>The word {0} can be stolen in these ways!</h2>
+#      {1}
+#  </body></html>
+#  """
 
 if __name__ == "__main__":
  # Launch the Flask dev server
